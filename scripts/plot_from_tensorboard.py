77e9112d8fa3c6478846cf6068d56385423ec1e8
Marcel Brucker <marcel.brucker@gmx.de>
2021-07-18 19:33:44 +0200

    corrected all_plots.py and new script to plot from tensorboard

--- scripts/plot_from_tensorboard.py
+++ scripts/plot_from_tensorboard.py
@@ -8,20 +8,22 @@ import pytablewriter
 import seaborn
 from matplotlib import pyplot as plt
 from scipy.spatial import distance_matrix
+from tensorboard.backend.event_processing.event_accumulator import EventAccumulator
+import pandas as pd
 
 parser = argparse.ArgumentParser("Gather results, plot them and create table")
 parser.add_argument("-a", "--algos", help="Algorithms to include", nargs="+", type=str)
 parser.add_argument("-e", "--env", help="Environments to include", nargs="+", type=str)
 parser.add_argument("-f", "--exp-folders", help="Folders to include", nargs="+", type=str)
 parser.add_argument("-l", "--labels", help="Label for each folder", nargs="+", type=str)
-parser.add_argument(
-    "-k",
-    "--key",
-    help="Key from the `evaluations.npz` file to use to aggregate results "
-    "(e.g. reward, success rate, ...), it is 'results' by default (i.e., the episode reward)",
-    default="results",
-    type=str,
-)
+# parser.add_argument(
+#     "-k",
+#     "--key",
+#     help="Key from the `evaluations.npz` file to use to aggregate results "
+#     "(e.g. reward, success rate, ...), it is 'results' by default (i.e., the episode reward)",
+#     default="results",
+#     type=str,
+# )
 parser.add_argument("-max", "--max-timesteps", help="Max number of timesteps to display", type=int, default=int(2e6))
 parser.add_argument("-min", "--min-timesteps", help="Min number of timesteps to keep a trial", type=int, default=-1)
 parser.add_argument("-o", "--output", help="Output filename (pickle file), where to save the post-processed data", type=str)
@@ -36,6 +38,7 @@ parser.add_argument(
 parser.add_argument(
     "-exp", "--experiment-names", help="Separate different experiments by checking their suffix", nargs="+", type=str, default="", 
 )
+parser.add_argument("-t", "--tag", help="" "", default="eval/mean_reward", type=str)
 args = parser.parse_args()
 
 # Activate seaborn
@@ -55,7 +58,7 @@ for env in args.env:  # noqa: C901
     x_label_suffix = "" if args.no_million else "(in Million)"
     plt.xlabel(f"Timesteps {x_label_suffix}", fontsize=14)
     # plt.ylabel("Score", fontsize=14)
-    plt.ylabel("Mean Reward", fontsize=14)
+    plt.ylabel(f"{args.tag}", fontsize=14)
     results[env] = {}
     post_processed_results[env] = {}
 
@@ -63,19 +66,29 @@ for env in args.env:  # noqa: C901
         for folder_idx, exp_folder in enumerate(args.exp_folders):
             for exp in args.experiment_names:
 
-                log_path = os.path.join(exp_folder, algo.lower())
+                log_path = os.path.join(exp_folder, "tensorboard")
 
                 if not os.path.isdir(log_path):
                     continue
-                
+
                 # What is this good for?
                 # results[env][f"{args.labels[folder_idx]}-{algo}"] = 0.0
+
+                for d in os.listdir(log_path):
+                    if (env in d and os.path.isdir(os.path.join(log_path, d))):
+                        log_path = os.path.join(log_path, d)
+                        break
+
+                if not(env in log_path):
+                    continue 
+
+                if algo == "MSAC":
+                    algo = "M-SAC"
 
                 dirs = [
                     os.path.join(log_path, d)
                     for d in os.listdir(log_path)
-                    # if (env in d and os.path.isdir(os.path.join(log_path, d)))
-                    if (env in d and d.endswith(exp) and os.path.isdir(os.path.join(log_path, d)))
+                    if (d.startswith(algo) and d.endswith(exp) and os.path.isdir(os.path.join(log_path, d)))
                 ]
 
                 max_len = 0
@@ -84,68 +97,47 @@ for env in args.env:  # noqa: C901
                 timesteps = np.empty(0)
                 for _, dir_ in enumerate(dirs):
                     try:
-                        log = np.load(os.path.join(dir_, "evaluations.npz"))
+                        log = EventAccumulator(dir_)
+                        log.Reload()
                     except FileNotFoundError:
-                        print("Eval not found for", dir_)
+                        print("Data not found for", dir_)
                         continue
-
-                    mean_ = np.squeeze(log["results"].mean(axis=1))
+                    
+                    # Somehow the last value is missing in the tensorboard values
+                    # and the steps are odd compared to evaluations.npz
+                    _, step_nums, mean_ = zip(*log.Scalars(args.tag))
+                    mean_ = np.array(mean_)
+                    step_nums = np.array(step_nums)
 
                     if mean_.shape == ():
                         continue
 
-                    max_len = max(max_len, len(mean_))
-                    if len(log["timesteps"]) >= max_len:
-                        timesteps = log["timesteps"]
-
                     # For post-processing
-                    merged_timesteps.append(log["timesteps"])
-                    merged_results.append(log[args.key])
+                    df = pd.DataFrame({'value': mean_}, index=step_nums)
+                    steps_resampled = np.arange(start=0, stop=args.max_timesteps, step=10000, dtype='int64')
+                    df_resampled = df.reindex(df.index.union(steps_resampled)).interpolate(method='values').loc[steps_resampled]
+                    step_nums = steps_resampled
+                    mean_ = df_resampled.to_numpy().squeeze()
+                    merged_timesteps.append(step_nums)
+                    merged_results.append(mean_)
+
+                    max_len = max(max_len, len(mean_))
+                    if len(step_nums) >= max_len:
+                        timesteps = step_nums
 
                     # Truncate the plots
+                    # max_len = min(len(i) for i in merged_timesteps) # old approach to synchronize
                     while timesteps[max_len - 1] > args.max_timesteps:
                         max_len -= 1
                     timesteps = timesteps[:max_len]
 
-                    if len(log[args.key]) >= max_len:
-                        last_eval.append(log[args.key][max_len - 1])
+                    if len(mean_) >= max_len:
+                        last_eval.append(mean_[max_len - 1])
                     else:
-                        last_eval.append(log[args.key][-1])
-
-                # Merge runs with different eval freq:
-                # ex: (100,) eval vs (10,)
-                # in that case, downsample (100,) to match the (10,) samples
-                # Discard all jobs that are < min_timesteps
-                min_trials = []
-                if args.min_timesteps > 0:
-                    min_ = np.inf
-                    for n_timesteps in merged_timesteps:
-                        if n_timesteps[-1] >= args.min_timesteps:
-                            min_ = min(min_, len(n_timesteps))
-                            if len(n_timesteps) == min_:
-                                max_len = len(n_timesteps)
-                                # Truncate the plots
-                                while n_timesteps[max_len - 1] > args.max_timesteps:
-                                    max_len -= 1
-                                timesteps = n_timesteps[:max_len]
-                    # Avoid modifying original aggregated results
-                    merged_results_ = deepcopy(merged_results)
-                    # Downsample if needed
-                    for trial_idx, n_timesteps in enumerate(merged_timesteps):
-                        # We assume they are the same, or they will be discarded in the next step
-                        if len(n_timesteps) == min_ or n_timesteps[-1] < args.min_timesteps:
-                            pass
-                        else:
-                            new_merged_results = []
-                            # Nearest neighbour
-                            distance_mat = distance_matrix(n_timesteps.reshape(-1, 1), timesteps.reshape(-1, 1))
-                            closest_indices = distance_mat.argmin(axis=0)
-                            for closest_idx in closest_indices:
-                                new_merged_results.append(merged_results_[trial_idx][closest_idx])
-                            merged_results[trial_idx] = new_merged_results
-                            last_eval[trial_idx] = merged_results_[trial_idx][closest_indices[-1]]
+                        last_eval.append(mean_[-1])
 
                 # Remove incomplete runs
+                # max_len = max(max_len, len(mean_))
                 merged_results_tmp, last_eval_tmp = [], []
                 for idx in range(len(merged_results)):
                     if len(merged_results[idx]) >= max_len:
@@ -178,8 +170,10 @@ for env in args.env:  # noqa: C901
                     # std: error:
                     std_error = std_ / np.sqrt(n_trials)
                     # Take last evaluation
+                    last_evals = np.array(last_eval).squeeze()
                     # shape: (n_trials, n_eval_episodes) to (n_trials,)
-                    last_evals = np.array(last_eval).squeeze().mean(axis=-1)
+                    if (type(last_eval[0]) is np.ndarray):
+                        last_evals = last_evals.mean(axis=-1)
                     # Standard deviation of the mean performance for the last eval
                     std_last_eval = np.std(last_evals)
                     # Compute standard error
@@ -187,12 +181,12 @@ for env in args.env:  # noqa: C901
 
                     if args.median:
                         # results[env][f"{algo}-{args.labels[folder_idx]}"] = f"{np.median(last_evals):.0f}"
-                        results[env][f"{algo}_{exp}-{args.labels[folder_idx]}"] = f"{np.median(last_evals):.0f}"
+                        results[env][f"{algo}_{exp}-{args.labels[folder_idx]}"] = f"{np.median(last_evals):.5f}"
                     else:
                         results[env][
                         #     f"{algo}-{args.labels[folder_idx]}"
                               f"{algo}_{exp}-{args.labels[folder_idx]}"
-                        ] = f"{np.mean(last_evals):.0f} +/- {std_error_last_eval:.0f}"
+                        ] = f"{np.mean(last_evals):.5f} +/- {std_error_last_eval:.5f}"
 
                     # x axis in Millions of timesteps
                     divider = 1e6
@@ -226,6 +220,8 @@ value_matrix[0].append("")
 for i, env in enumerate(args.env, start=1):
     value_matrix[i].append(env)
     for algo in args.algos:
+        if algo == "MSAC":
+            algo = "M-SAC"
         for label in args.labels:
             for exp in args.experiment_names:
                 key = f"{algo}_{exp}-{label}"
@@ -248,3 +244,18 @@ if args.output is not None:
 
 if not args.no_display:
     plt.show()
+
+"""
+event_acc = EventAccumulator('/Users/Marcel/Repositories/tum-adlr-ss21-08/docs/results/archive/tensorboard/AntBulletEnv-v0/M-SAC_1')
+event_acc.Reload()
+# Show all tags in the log file
+for key, value in event_acc.Tags().items():
+    print(key)
+    print(value)
+# print(event_acc.Tags())
+
+# E. g. get wall clock, number of steps and value for a scalar 'Accuracy'
+temp = event_acc.Scalars('eval/mean_reward')
+w_times, step_nums, vals = zip(*event_acc.Scalars('eval/mean_reward'))
+print("Hello!")
+"""
